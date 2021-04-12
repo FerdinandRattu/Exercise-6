f72c0c2c3761dd0b83df16419406b78ff56c2946
nand19 <s11810014@student.unklab.ac.id>
2021-04-13 02:27:57 +0800

    Exercise 6

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import Register from './components/screens/Register';
+import UserList from './components/screens/UserList';
+
+const App = () => {
+  return (
+    <>
+      {/* <Register/> */}
+      <UserList/>
+    </>
+  )
+}
+
+export default App;
+
+const styles = StyleSheet.create({})
